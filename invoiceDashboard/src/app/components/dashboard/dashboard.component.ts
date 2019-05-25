e3da0ceff8c095bf1f6ded1989720037fb6355d8
Muiz Junaid Khan <mzkhan25@hotmail.com>
2019-05-25 16:58:54 +0500

    Invoice add implemented

--- invoiceDashboard/src/app/components/dashboard/dashboard.component.ts
+++ invoiceDashboard/src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Invoice } from 'src/app/models/invoice';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store/app.state';
 import { Router } from '@angular/router';
 import { InvoiceDialogComponent } from '../invoice-dialog/invoice-dialog.component';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,18 +17,13 @@ export class DashboardComponent implements OnInit {
   invoices: Observable<Invoice[]>;
   displayedColumns: string[] = ['date', 'title', 'amount', 'iban'];
   dataSource: any;
-  animal: string;
-  name: string;
-  constructor(private router: Router, private store: Store<AppState>, public dialog: MatDialog) {}
+  constructor(private router: Router, private store: Store<AppState>, public dialog: MatDialog) { }
   openDialog(): void {
-    const dialogRef = this.dialog.open(InvoiceDialogComponent, {
-      width: '250px',
-      data: {name: this.name, animal: this.animal}
-    });
+    const dialogRef = this.dialog.open(InvoiceDialogComponent);
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      this.animal = result;
+
     });
   }
   addNew() {
@@ -41,20 +36,3 @@ export class DashboardComponent implements OnInit {
   }
 
 }
-// @Component({
-//   selector: 'app-invoice-dialog',
-//   templateUrl: 'invoice-dialog.html',
-// })
-
-
-// export class InvoiceDialogComponent {
-
-//   constructor(
-//     public dialogRef: MatDialogRef<InvoiceDialogComponent>,
-//     @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
-
-//   onNoClick(): void {
-//     this.dialogRef.close();
-//   }
-
-// }
--- invoiceDashboard/src/app/components/invoice-dialog/invoice-dialog.component.ts
+++ invoiceDashboard/src/app/components/invoice-dialog/invoice-dialog.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
-
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.state';
+import * as InvoiceActions from '../../store/actions/invoice.actions';
+import { Invoice } from 'src/app/models/invoice';
 export interface DialogData {
   animal: string;
   name: string;
@@ -14,16 +17,27 @@ export interface DialogData {
 })
 
 export class InvoiceDialogComponent implements OnInit {
-
+  selectedTab = 0;
+  date: any;
+  title: string;
+  amount: number;
+  iban: string;
+  invoice: Invoice;
   constructor(
     public dialogRef: MatDialogRef<InvoiceDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, private store: Store<AppState>) { }
+
 
   onNoClick(): void {
     this.dialogRef.close();
   }
-
-  ngOnInit() {
+  changeTab() {
+    this.selectedTab = 1;
   }
+  addNewPayment(id, title, date, amount, iban) {
+    this.store.dispatch(new InvoiceActions.AddInvoice({id, title, amount, date, iban}) );
+    this.dialogRef.close();
+  }
+  ngOnInit() {}
 
 }
